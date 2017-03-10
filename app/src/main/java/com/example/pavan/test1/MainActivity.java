a3cd27e46819f6b16809c5a6411800f3dba193b1
Pavan <kpavandev@gmail.com>
2017-03-11 00:11:11 +0530

    added the file

--- app/src/main/java/com/example/pavan/test1/MainActivity.java
+++ app/src/main/java/com/example/pavan/test1/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.pavan.test1;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.MenuItemWrapperICS;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.pavan.test1.modles.MovieModel;
@@ -108,11 +117,70 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(List<MovieModel> s) {
             super.onPostExecute(s);
-
+            MovieAdapter adpater = new MovieAdapter(getApplicationContext(),R.layout.row,s);
+            listView.setAdapter(adpater);
 
         }
     }
 
+    public class MovieAdapter extends ArrayAdapter  {
+        private final int resource;
+        private List<MovieModel> movieModelList;
+        private LayoutInflater inflater;
+        private MovieAdapter(Context context,int resource, List<MovieModel> objects) {
+            super(context, resource, objects);
+            movieModelList = objects;
+            this.resource = resource;
+            inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if(convertView== null)
+            {
+                convertView = inflater.inflate(R.layout.row,null);
+            }
+            ImageView imageview;
+            TextView txtmovie;
+            TextView txttagline;
+            TextView txtyear;
+            TextView txtduration;
+            TextView txtdirector;
+            TextView txtcast;
+            TextView txtstory;
+            RatingBar rtbar;
+
+            imageview = (ImageView)convertView.findViewById(R.id.imageView);
+            txtmovie =(TextView)convertView.findViewById(R.id.movienameid);
+            txttagline=(TextView)convertView.findViewById(R.id.taglineid);
+            txtyear=(TextView)convertView.findViewById(R.id.yearid);
+            txtduration=(TextView)convertView.findViewById(R.id.durationid);
+            txtdirector=(TextView)convertView.findViewById(R.id.directorid);
+            txtcast=(TextView)convertView.findViewById(R.id.castid);
+            txtstory=(TextView)convertView.findViewById(R.id.storyid);
+            rtbar =(RatingBar)convertView.findViewById(R.id.ratingbar);
+
+            txtmovie.setText(movieModelList.get(position).getMovie());
+            txttagline.setText("Year: "+movieModelList.get(position).getTagline());
+            txtyear.setText(movieModelList.get(position).getYear());
+            txtdirector.setText(movieModelList.get(position).getDirector());
+            txtduration.setText(movieModelList.get(position).getDuration());
+
+            StringBuffer stringbuffer = new StringBuffer();
+
+            for(MovieModel.Cast cast:movieModelList.get(position).getCastList())
+            {
+                stringbuffer.append(cast.getName()+",");
+            }
+            txtcast.setText(stringbuffer);
+            txtstory.setText(movieModelList.get(position).getStory());
+
+            //Rating bar
+            rtbar.setRating(movieModelList.get(position).getRating()/2);
+            return  convertView;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_activity, menu);

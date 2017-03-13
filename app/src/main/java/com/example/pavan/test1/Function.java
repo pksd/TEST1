6e972fb90c6d48bda17c584df9eb9e76db4d154c
Pavan <kpavandev@gmail.com>
2017-03-14 00:59:43 +0530

    done

---
+++ app/src/main/java/com/example/pavan/test1/Function.java
@@ -0,0 +1,9 @@
+package com.example.pavan.test1;
+
+/**
+ * Created by pavan on 13/3/17.
+ */
+
+public class Function {
+
+}
--- app/src/main/java/com/example/pavan/test1/MainActivity.java
+++ app/src/main/java/com/example/pavan/test1/MainActivity.java
@@ -1,26 +1,19 @@
 package com.example.pavan.test1;
 
-import android.content.Context;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.menu.MenuItemWrapperICS;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.RatingBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
-
-import com.example.pavan.test1.modles.MovieModel;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,33 +23,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ProgressDialog pDialog;
     private TextView txt;
-    private ListView listView;
-
+    private ListView lv;
+    public static final String Urlis =" http://api1.yuktix.com:8080/sensordb/v1/module/device/archive/latest";
+    public static final String Ukey="1868cac0-a92b-4b17-9cef-c540274f9cf0";
+    ArrayList<HashMap<String, String>> contactList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView)findViewById(R.id.movies);
-
-               // new JSONTask().execute("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesDemoItem.txt");
+        Button btn =(Button)findViewById(R.id.button);
+        txt = (TextView) findViewById(R.id.text1);
+        lv = (ListView) findViewById(R.id.list_item1);
+        contactList = new ArrayList<>();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JSONTask().execute(" http://api1.yuktix.com:8080/sensordb/v1/module/device/archive/latest");
+            }
+        });
     }
-     public class JSONTask extends AsyncTask<String,String,List<MovieModel>>{
+    public class JSONTask extends AsyncTask<String,String,String>{
         @Override
-        protected List<MovieModel> doInBackground(String... params) {
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
             BufferedReader reader = null;
             HttpURLConnection connection=null;
+           // String input="{\"map\": {\"name\" : \"AWS\"}}";
+            String ip ="{\"map\" : {\"module\": \"AWS\" , \"serialNumber\" : \"yuktix1\", \"limit\" : \"1\" }}";
+
+
             try {
-                URL url = new URL(params[0]);
+                URL url = new URL(Urlis);
                 connection =(HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Authorization","Signature="+Ukey);
+                OutputStream os =connection.getOutputStream();
+                os.write(ip.getBytes());
+                os.flush();
                 connection.connect();
                 InputStream stream = connection.getInputStream();
                 reader = new BufferedReader(new InputStreamReader(stream));
@@ -65,34 +90,24 @@ public class MainActivity extends AppCompatActivity {
                 while ((line = reader.readLine())!= null){
                     buffer.append(line);
                 }
-                String finalJSON =buffer.toString();
-                JSONObject object = new JSONObject(finalJSON);
-                JSONArray array = object.getJSONArray("movies");
-                List <MovieModel> movieModelslist= new ArrayList<>();
-                for(int i=0;i<array.length();i++){
-                    JSONObject jsonObject =array.getJSONObject(i);
-                    MovieModel movieModel =new MovieModel();
-                    movieModel.setMovie(jsonObject.getString("movie"));
-                    movieModel.setYear( jsonObject.getInt("year"));
-                    movieModel.setRating((float)jsonObject.getDouble("rating"));
-                    movieModel.setDirector(jsonObject.getString("director"));
-                    movieModel.setDuration(jsonObject.getString("duration"));
-                    movieModel.setTagline(jsonObject.getString("tagline"));
-                    movieModel.setImage(jsonObject.getString("image"));
-                    movieModel.setStory(jsonObject.getString("story"));
-                    List<MovieModel.Cast> castList=  new ArrayList<>();
-                    for(int j=0;j<jsonObject.getJSONArray("cast").length();j++){
-                        JSONObject jsonObject1 = jsonObject.getJSONArray("cast").getJSONObject(j) ;
-                        MovieModel.Cast cast = new MovieModel.Cast();
-                        cast.setName(jsonObject1.getString("name"));
-                        castList.add(cast);
-                    }
-                    movieModel.setCastList(castList);
-                    movieModelslist.add(movieModel);
-                }
-
-                return movieModelslist;
-
+                String finalJson = buffer.toString();
+                JSONObject parentobject = new JSONObject(finalJson);
+                JSONArray parentarray = parentobject.getJSONArray("result");
+                //for (int i = 0; i < parentarray.length(); i++) {
+                    JSONObject finalobj = parentarray.getJSONObject(0);
+                    String ids = finalobj.getString("id");
+                    // int yr  =finalobj.getInt("id");
+                    String tempereature = finalobj.getString("T");
+                    String humidity = finalobj.getString("RH");
+                    String rain = finalobj.getString("Rain");
+                    HashMap<String, String> contact = new HashMap<>();
+                    contact.put("ids",ids);
+                    contact.put("temperature",tempereature);
+                    contact.put("humidity",humidity);
+                    contact.put("rain",rain);
+                    contactList.add(contact);
+                    //return moviename + "-" + email;
+               //}
             }catch (MalformedURLException e){
                 e.printStackTrace();
             } catch (IOException e) {
@@ -115,85 +130,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(List<MovieModel> s) {
+        protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            MovieAdapter adpater = new MovieAdapter(getApplicationContext(),R.layout.row,s);
-            listView.setAdapter(adpater);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList, R.layout.list_item,
+                    new String[]{"ids", "temperature","humidity","Rain"}, new int[]{R.id.aws_id, R.id.temperature,R.id.humid,R.id.rain});
 
+            lv.setAdapter(adapter);
         }
     }
 
-    public class MovieAdapter extends ArrayAdapter  {
-        private final int resource;
-        private List<MovieModel> movieModelList;
-        private LayoutInflater inflater;
-        private MovieAdapter(Context context,int resource, List<MovieModel> objects) {
-            super(context, resource, objects);
-            movieModelList = objects;
-            this.resource = resource;
-            inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            if(convertView== null)
-            {
-                convertView = inflater.inflate(R.layout.row,null);
-            }
-            ImageView imageview;
-            TextView txtmovie;
-            TextView txttagline;
-            TextView txtyear;
-            TextView txtduration;
-            TextView txtdirector;
-            TextView txtcast;
-            TextView txtstory;
-            RatingBar rtbar;
-
-            imageview = (ImageView)convertView.findViewById(R.id.imageView);
-            txtmovie =(TextView)convertView.findViewById(R.id.movienameid);
-            txttagline=(TextView)convertView.findViewById(R.id.taglineid);
-            txtyear=(TextView)convertView.findViewById(R.id.yearid);
-            txtduration=(TextView)convertView.findViewById(R.id.durationid);
-            txtdirector=(TextView)convertView.findViewById(R.id.directorid);
-            txtcast=(TextView)convertView.findViewById(R.id.castid);
-            txtstory=(TextView)convertView.findViewById(R.id.storyid);
-            rtbar =(RatingBar)convertView.findViewById(R.id.ratingbar);
-
-            txtmovie.setText(movieModelList.get(position).getMovie());
-            txttagline.setText("Year: "+movieModelList.get(position).getTagline());
-            txtyear.setText(movieModelList.get(position).getYear());
-            txtdirector.setText(movieModelList.get(position).getDirector());
-            txtduration.setText(movieModelList.get(position).getDuration());
-
-            StringBuffer stringbuffer = new StringBuffer();
-
-            for(MovieModel.Cast cast:movieModelList.get(position).getCastList())
-            {
-                stringbuffer.append(cast.getName()+",");
-            }
-            txtcast.setText(stringbuffer);
-            txtstory.setText(movieModelList.get(position).getStory());
-
-            //Rating bar
-            rtbar.setRating(movieModelList.get(position).getRating()/2);
-            return  convertView;
-        }
-    }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_activity, menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
         return true;
     }
 
     @Override
-    public  boolean onOptionsItemSelected(MenuItem item){
-        int id= item.getItemId();
-        if (id==R.id.actionsetting) {
-            new JSONTask().execute("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesData.txt");
-            return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_setting:
+                //Toast.makeText(getApplicationContext(),"Refresh Clicked",Toast.LENGTH_LONG).show();
+                new JSONTask().execute();
+                break;
+            case R.id.action_test:
+                startActivity(new Intent("com.example.pavan.test1.Main2Activity"));
+                break;
+            case R.id.action_test2:
+                Toast.makeText(getApplicationContext(),"Clicked 2nd",Toast.LENGTH_LONG).show();
+                break;
         }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
